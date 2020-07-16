b4e75f7d789e0b57cb498c278796394b5f77d4b6
Inna_Alex <inna.aleksandrycheva@gmail.com>
2020-07-16 23:41:28 +0300

    Add API using Django Rest Framework

---
+++ catalog/category_api_views.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+
+from .models import Category
+from .serializers import CategorySerializer
+
+# CRUD
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
